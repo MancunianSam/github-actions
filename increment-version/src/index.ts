edecc41fe29b19a8cf552449ec9e964d6cf234b2
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-01 18:07:32 +0000

    Add print statement

--- increment-version/src/index.ts
+++ increment-version/src/index.ts
@@ -8,5 +8,6 @@ const octokit = new Octokit()
 type ListTagsResponseType = GetResponseTypeFromEndpointMethod<typeof octokit.repos.listTags>;
 
 axios.get<ListTagsResponseType>(`https://api.github.com/repos/nationalarchives/tdr-${repoName}/tags`).then(res => {
+  console.log(res.data.data)
   setOutput("next_version", (parseInt(res.data.data[0].name) + 1).toString())
 }).catch(err => setFailed(err))
