fdb556420271d687454bf235a66e7acfdce3ea88
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-01 16:26:31 +0000

    Add increment version script

---
+++ increment-version/src/index.ts
@@ -0,0 +1,13 @@
+import { getInput, setOutput, setFailed } from "@actions/core"
+import { context } from "@actions/github"
+import axios from "axios";
+import { Octokit } from "@octokit/rest";
+import { GetResponseTypeFromEndpointMethod } from "@octokit/types";
+
+const repoName = getInput("repoName")
+const octokit = new Octokit()
+type ListTagsResponseType = GetResponseTypeFromEndpointMethod<typeof octokit.repos.listTags>;
+
+axios.get<ListTagsResponseType>(`${context.apiUrl}/repos/nationalarchives/tdr-${repoName}/tags`).then(res => {
+  setOutput("nextVersion", (parseInt(res.data.data[0].name) + 1).toString())
+}).catch(err => setFailed(err))
