d897f6412968bd6b926ad3564ddfcea8a15f94c2
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-01 18:17:32 +0000

    Use github rest

--- increment-version/src/index.ts
+++ increment-version/src/index.ts
@@ -1,13 +1,9 @@
 import { getInput, setOutput, setFailed } from "@actions/core"
-import axios from "axios";
 import { Octokit } from "@octokit/rest";
-import { GetResponseTypeFromEndpointMethod } from "@octokit/types";
 
-const repoName = getInput("repo_name")
+const repo = getInput("repo_name")
 const octokit = new Octokit()
-type ListTagsResponseType = GetResponseTypeFromEndpointMethod<typeof octokit.repos.listTags>;
 
-axios.get<ListTagsResponseType>(`https://api.github.com/repos/nationalarchives/tdr-${repoName}/tags`).then(res => {
-  console.log(res.data.data)
-  setOutput("next_version", (parseInt(res.data.data[0].name) + 1).toString())
+octokit.repos.listTags({repo, owner: "nationalarchives"}).then(res => {
+  setOutput("next_version", (parseInt(res.data[0].name) + 1).toString())
 }).catch(err => setFailed(err))
