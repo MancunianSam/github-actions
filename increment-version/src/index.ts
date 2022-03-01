b581e9cc70896cca41be01f792035c8cca6b7d0a
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-01 18:01:49 +0000

    Try using separate actions

--- increment-version/src/index.ts
+++ increment-version/src/index.ts
@@ -1,5 +1,4 @@
 import { getInput, setOutput, setFailed } from "@actions/core"
-import { context } from "@actions/github"
 import axios from "axios";
 import { Octokit } from "@octokit/rest";
 import { GetResponseTypeFromEndpointMethod } from "@octokit/types";
@@ -8,6 +7,6 @@ const repoName = getInput("repo_name")
 const octokit = new Octokit()
 type ListTagsResponseType = GetResponseTypeFromEndpointMethod<typeof octokit.repos.listTags>;
 
-axios.get<ListTagsResponseType>(`${context.apiUrl}/repos/nationalarchives/tdr-${repoName}/tags`).then(res => {
+axios.get<ListTagsResponseType>(`https://api.github.com/repos/nationalarchives/tdr-${repoName}/tags`).then(res => {
   setOutput("next_version", (parseInt(res.data.data[0].name) + 1).toString())
 }).catch(err => setFailed(err))
