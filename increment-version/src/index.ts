cd63944506815839b26cf2765f9266e2ed4b8dcb
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-01 19:37:37 +0000

    Proper github action

--- increment-version/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { getInput, setOutput, setFailed } from "@actions/core"
-import { Octokit } from "@octokit/rest";
-
-const repo = getInput("repo_name")
-const octokit = new Octokit()
-
-octokit.repos.listTags({repo, owner: "nationalarchives"}).then(res => {
-  setOutput("next_version", `v${parseInt(res.data[0].name.slice(1)) + 1}`)
-}).catch(err => setFailed(err))
---
+++ increment-version/src/run.ts
@@ -0,0 +1,17 @@
+import {Octokit} from "@octokit/rest";
+import {getInput, setFailed, setOutput} from "@actions/core";
+
+export async function run(): Promise<void> {
+  const octokit = new Octokit()
+  const repo = getInput('repo_name')
+  try {
+    const {data} = await octokit.repos.listTags({
+      repo,
+      owner: 'nationalarchives'
+    })
+    const nextVersion = `v${parseInt(data[0].name.slice(1)) + 1}`
+    setOutput('next_version', nextVersion)
+  } catch (err) {
+    setFailed((err as Error).message)
+  }
+}
