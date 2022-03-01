47e88f03880e3bacbed7ff9dddab866daee98d5c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-01 16:45:22 +0000

    Add reusable build job

--- increment-version/src/index.ts
+++ increment-version/src/index.ts
@@ -4,10 +4,10 @@ import axios from "axios";
 import { Octokit } from "@octokit/rest";
 import { GetResponseTypeFromEndpointMethod } from "@octokit/types";
 
-const repoName = getInput("repoName")
+const repoName = getInput("repo_name")
 const octokit = new Octokit()
 type ListTagsResponseType = GetResponseTypeFromEndpointMethod<typeof octokit.repos.listTags>;
 
 axios.get<ListTagsResponseType>(`${context.apiUrl}/repos/nationalarchives/tdr-${repoName}/tags`).then(res => {
-  setOutput("nextVersion", (parseInt(res.data.data[0].name) + 1).toString())
+  setOutput("next_version", (parseInt(res.data.data[0].name) + 1).toString())
 }).catch(err => setFailed(err))
